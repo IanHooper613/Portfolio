19b9fc8bca8937166ec411bc419d3cad4286d0c3
chizz <cbrittingham@gmail.com>
2020-04-22 22:48:08 -0500

    Updates

---
+++ precache-manifest.8425c738775177479dfbe1c694bc9953.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e34dabd1e3777b249912d52f73e76ba9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d9c5764619c5a829f17",
+    "url": "/portfolio/static/css/main.5b82a24b.chunk.css"
+  },
+  {
+    "revision": "892ef5cf1bcd0d0e2b61",
+    "url": "/portfolio/static/js/2.953b7756.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.953b7756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9c5764619c5a829f17",
+    "url": "/portfolio/static/js/main.8323b860.chunk.js"
+  },
+  {
+    "revision": "7e84dee687ac6de54091",
+    "url": "/portfolio/static/js/runtime-main.4da689ad.js"
+  }
+]);
