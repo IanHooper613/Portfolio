ad131c556dfa23c0b49e44eaf77bb193a8ee644e
chizz <cbrittingham@gmail.com>
2020-04-26 11:37:31 -0500

    changed a to Link, fixed import in App

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-import { Router as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
@@ -11,14 +11,14 @@ import { StoreProvider } from './utils/GlobalState';
 
 function App() {
   return (
-    <Router basename='/'>
+    <Router>
       <div>
         <StoreProvider>
           <NavBar />
           <Switch>
-            <Route exact path='/portfolio' component={Home} />
-            <Route exact path='/portfolio/contact' component={Contact} />
-            <Route exact path='/portfolio/portfolio' component={Portfolio} />
+            <Route exact path='/' component={Home} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/portfolio/' component={Portfolio} />
           </Switch>
           <Footer />
         </StoreProvider>
--- react-portfolio/src/components/NavBar/index.js
+++ react-portfolio/src/components/NavBar/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function NavBar () {
     return(
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-          <a className="navbar-brand" href="/">Charlie</a>
+          <Link className="navbar-brand" to="/">Charlie</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -11,13 +12,13 @@ function NavBar () {
             <ul className="navbar-nav">
               <li className="nav-item">
                 
-                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
               </li>
               <li className="nav-item">
-                  <a className="nav-link" href="/portfolio">Portfolio</a>
+                  <Link className="nav-link" to="/portfolio">Portfolio</Link>
                 </li>
               <li className="nav-item">
-                <a className="nav-link" href="/contact">Contact</a>
+                <Link className="nav-link" to="/contact">Contact</Link>
               </li>
             </ul>
           </div>
