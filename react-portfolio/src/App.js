507967a8b627fb7839162982739e401d9127793e
chizz <cbrittingham@gmail.com>
2020-04-26 16:25:28 -0500

    changed routing again

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import logo from './logo.svg';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
@@ -13,19 +13,19 @@ const URL = process.env.BASE_URL; //for use on gh pages
 function App() {
   console.log('URL ', process.env);
   return (
-    <Router>
+    <HashRouter basename='/'>
       <div>
         <StoreProvider>
           <NavBar />
           <Switch>
-            <Route exact path={URL + '/'} component={Home} />
-            <Route exact path={URL + '/contact'} component={Contact} />
-            <Route exact path={URL + '/portfolio'} component={Portfolio} />
+            <Route exact path= '/'component={Home} />
+            <Route exact path= '/contact'component={Contact} />
+            <Route exact path= '/portfolio' component={Portfolio} />
           </Switch>
           <Footer />
         </StoreProvider>
       </div>
-    </Router>
+    </HashRouter>
   );
 };
 export default App;
