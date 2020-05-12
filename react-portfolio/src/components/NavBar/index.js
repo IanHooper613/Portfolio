a26b74209e17fbdc5e6071a7d49038e89fe91494
chizz <cbrittingham@gmail.com>
2020-05-12 17:13:16 -0500

    updated link on Charlie button to redirect to homepage

--- react-portfolio/src/components/NavBar/index.js
+++ react-portfolio/src/components/NavBar/index.js
@@ -4,22 +4,21 @@ const URL = process.env.PUBLIC_URL;
 
 function NavBar () {
     return(
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-          <Link className="navbar-brand" to="/">Charlie</Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+        <nav className='navbar navbar-expand-sm navbar-dark bg-dark'>
+          <Link className='navbar-brand' to={URL + '/'}>Charlie</Link>
+          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon'></span>
           </button>
-          <div className="collapse navbar-collapse flex-row-reverse align-content-sm-center" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                
-                <Link className="nav-link" to={URL + "/"}>Home <span className="sr-only">(current)</span></Link>
+          <div className='collapse navbar-collapse flex-row-reverse align-content-sm-center' id='navbarNav'>
+            <ul className='navbar-nav'>
+              <li className='nav-item'>
+                <Link className='nav-link' to={URL + '/'}>Home <span className='sr-only'>(current)</span></Link>
               </li>
-              <li className="nav-item">
-                  <Link className="nav-link" to={URL + "/portfolio"}>Portfolio</Link>
+              <li className='nav-item'>
+                  <Link className='nav-link' to={URL + '/portfolio'}>Portfolio</Link>
                 </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={URL + "/contact"}>Contact</Link>
+              <li className='nav-item'>
+                <Link className='nav-link' to={URL + '/contact'}>Contact</Link>
               </li>
             </ul>
           </div>
