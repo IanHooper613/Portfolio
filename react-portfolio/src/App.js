97d05ed790adce5191b53f6b607fb6ce3df4204c
chizz <cbrittingham@gmail.com>
2020-06-17 13:46:22 -0500

    removed contact page

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
 import Portfolio from './pages/Portfolio';
 import Footer from './components/Footer';
 import { StoreProvider } from './utils/GlobalState';
@@ -20,7 +19,6 @@ function App() {
           <NavBar />
           <Switch>
             <Route exact path= {URL + '/'} component={Home} />
-            <Route exact path= {URL + '/contact'} component={Contact} />
             <Route exact path= {URL + '/portfolio'} component={Portfolio} />
           </Switch>
           <Footer />
